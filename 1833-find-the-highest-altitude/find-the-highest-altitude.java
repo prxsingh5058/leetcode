8b2c3d97c79605f8d904a16e4f3caf4d27c0e903
prxsingh5058 <208329098+prxsingh5058@users.noreply.github.com>
2026-07-31 22:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (19.14%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int alt = 0;
+        int maxAlt = 0;
+
+        for(int i = 0; i <gain.length; i++) {
+            alt = alt + gain[i];
+
+            if(alt > maxAlt) {
+                maxAlt = alt;
+            }
+
+        }
+        return maxAlt;
+    }
+}
