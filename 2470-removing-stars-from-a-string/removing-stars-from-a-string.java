170969fc2b70ca23156c365f602c952007d9304c
prxsingh5058 <208329098+prxsingh5058@users.noreply.github.com>
2026-08-06 18:57:53 +0530

    Time: 29 ms (84.60%) | Memory: 48.1 MB (61.52%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeStars(String s) {
+        
+        StringBuilder ans = new StringBuilder();
+
+        for(char c : s.toCharArray()) {
+            if(c == '*') {
+                ans.deleteCharAt(ans.length() - 1);
+            }
+            else {
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+}
